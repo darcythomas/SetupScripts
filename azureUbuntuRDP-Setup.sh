71105032c9dfe786b2ab8745187b0107cadd971d
darcythomas <github@darcy.io>
2014-01-11 14:36:39 +1300

    adding color codes

--- azureUbuntuRDP-Setup.sh
+++ azureUbuntuRDP-Setup.sh
@@ -1,20 +1,24 @@
 #!/usr/bin/env bash
+   
+Red          ='\e[0;31'    
+NC           ='\e[0m' # No Color
 
-echo place to contain the mess
+echo -e "${red}place to contain the mess${NC}"
 mkdir installSetup
 cd installSetup
 
-
-echo get files
+echo -e "${red}get files${NC}"
 wget --no-check-certificate https://raw2.github.com/darcythomas/SetupScripts/master/apt-getInstall.sh 
+wget --no-check-certificate https://raw2.github.com/darcythomas/SetupScripts/master/colorCodes.sh
 
 
-echo run scripts
-bash apt-getInstall.sh
+echo -e "${red}run scripts${NC}"
+./colorCodes.sh
+./apt-getInstall.sh
 
 
 
 
-echo clean up
+echo -e "${red}clean up${NC}"
 cd ..
 rm -r installSetup
