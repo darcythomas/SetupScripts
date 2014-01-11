5a22f52ae4ecdfbe1a1e079d88bc01cc87337468
darcythomas <github@darcy.io>
2014-01-11 15:35:47 +1300

    fixing spelling

--- azureUbuntuRDP-Setup.sh
+++ azureUbuntuRDP-Setup.sh
@@ -41,7 +41,7 @@ cd installSetup
 
 echo get files | colorize RED
 #wget --no-check-certificate https://raw2.github.com/darcythomas/SetupScripts/master/apt-getInstall.sh && bash ./apt-getInstall.sh
-wget --no-check-certificate https://raw2.github.com/darcythomas/SetupScripts/master/vncserversetup.sh && bash ./vncserversetup.sh
+wget --no-check-certificate https://raw2.github.com/darcythomas/SetupScripts/master/vncserverSetup.sh && bash ./vncserverSetup.sh
 
 
 #echo Permission scripts | colorize RED
