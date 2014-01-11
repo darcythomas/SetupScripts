1668b8cd20432e4e42301d6fc2134526d75d5ec4
darcythomas <github@darcy.io>
2014-01-11 15:00:20 +1300

    messing with color

--- azureUbuntuRDP-Setup.sh
+++ azureUbuntuRDP-Setup.sh
@@ -1,28 +1,9 @@
 #!/usr/bin/env bash
    
-Red          ='\e[0;31'    
-NC           ='\e[0m' # No Color
+Red          ='\033[0;31'    
+NC           ='\033[0m' # No Color
 
-# colorize stdin according to parameter passed (GREEN, CYAN, BLUE, YELLOW)
-colorize(){
-    GREEN="\033[0;32m"
-    CYAN="\033[0;36m"
-    GRAY="\033[0;37m"
-    BLUE="\033[0;34m"
-    YELLOW="\033[0;33m"
-    NORMAL="\033[m"
-    color=\$${1:-NORMAL}
-    # activate color passed as argument
-    echo -ne "`eval echo ${color}`"
-    # read stdin (pipe) and print from it:
-    cat
-    # Note: if instead of reading from the pipe, you wanted to print
-    # the additional parameters of the function, you could do:
-    # shift; echo $*
-    # back to normal (no color)
-    echo -ne "${NORMAL}"
-}
-echo hi | colorize GREEN
+
 
 
 echo -e "${red}place to contain the mess${NC}"
@@ -41,7 +22,7 @@ echo -e "${red}run scripts${NC}"
 
 
 
-echo hi | colorize GREEN
+#echo hi | colorize GREEN
 echo -e "${red}clean up${NC}"
 cd ..
 rm -r installSetup
